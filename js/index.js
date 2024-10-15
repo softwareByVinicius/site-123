292a5f1d9cdfd95b2805d8693b1209382da2ee12
ViniciusBenetti <makako1289@gmail.com>
2024-10-15 11:42:08 -0300

    get

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 const menuOpened = document.querySelector("header i");
 const menuClosed = document.querySelector(".menu i");
+const returnToMain = document.querySelector(".return");
 
 
 
 function openMenu(){
     const menu = document.querySelector(".menu");
-    menu.style.transform = "translateX(0vw)"
-    menu.style.transition = "all 0.5s ease-in-out"
-    menu.style.visibility = "visible"
+    menu.style.transform = "translateX(0vw)";
+    menu.style.transition = "all 0.5s ease-in-out";
+    menu.style.visibility = "visible";
 }
 function closeMenu(){
     const menu = document.querySelector(".menu");
-    menu.style.transition = "all 0.5s ease-in-out"
-    menu.style.transform = "translateX(100vw)"
-    menu.style.visibility = "hidden"
+    menu.style.transition = "all 0.5s ease-in-out";
+    menu.style.transform = "translateX(100vw)";
+    menu.style.visibility = "hidden";
 
+}
+function procedureReturnToMain(){
+    location.href = location.origin;
 }
